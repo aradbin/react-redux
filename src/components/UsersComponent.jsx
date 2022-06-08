b17a5eca995706dc27f84cd3a479db04a358b1bf
aradbin <aradbin@gmail.com>
2022-06-08 17:48:47 +0600

    redux, redux-saga

---
+++ src/components/UsersComponent.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../redux/user/actions";
+import Card from "./CardComponent";
+
+const Users = () => {
+    const dispatch = useDispatch();
+    const users = useSelector(state => state.users.users);
+    const loading = useSelector(state => state.users.loading);
+    const error = useSelector(state => state.users.error);
+
+
+    useEffect(() => {
+        dispatch(getUsers());
+    },[]);
+
+    function refreshUser(){
+        dispatch(getUsers());
+    }
+
+    return (
+        <>
+            <button onClick={refreshUser}>Refresh</button>
+            {users.loading && <p>Loading...</p>}
+            {users.length === 0 && !loading && <p>No users available!</p>}
+            {error && !loading && <p>{error}</p>}
+            {users.length > 0 && users.map((user) => (
+                <Card key={user.id} user={user} />
+            ))}
+        </>
+    );
+}
+
+export default Users;
